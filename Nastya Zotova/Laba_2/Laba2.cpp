c0c96c85d44660fd9105b7b545c789d70166373e
nastyaz122 <115534094+nastyaz122@users.noreply.github.com>
2022-10-11 11:13:07 +0300

    Create Laba2.cpp

---
+++ Nastya Zotova/Laba_2/Laba2.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	int a[100], n, c, i;
+
+	cout << "VVedite razmer massiva" << endl;
+
+	cin >> n;
+
+	cout << "VVedire elementi massiva" << endl;
+
+	for (i = 0; i < n; i++) {
+
+		cin >> a[i];
+
+	}
+
+	cout << endl;
+
+	for (int i = n - 1; i >= 0; i--) {
+
+		cout << a[i] << " ";
+	}
+
+	return 0;
+
+}
