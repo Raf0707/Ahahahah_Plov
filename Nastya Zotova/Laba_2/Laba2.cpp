d6a0f495e5b89a13d6160295b669c5d50685d1cf
nastyaz122 <115534094+nastyaz122@users.noreply.github.com>
2022-10-12 15:11:17 +0300

    Update Laba2.cpp

--- Nastya Zotova/Laba_2/Laba2.cpp
+++ Nastya Zotova/Laba_2/Laba2.cpp
@@ -4,32 +4,42 @@ using namespace std;
 
 int main()
 {
-	int a[100], res[100], n, i;
-
-	cout << "VVedite razmer massiva" << endl;
-
-	cin >> n;
-
-	cout << "VVedite elementi massiva" << endl;
-
-	for (i = 0; i < n; i++) {
-
-		cin >> a[i];
-
-	}
-
-	cout << endl;
-
-	for (int i = n - 1; i >= 0; i--) {
-                //res.add(a[i]);
-
-		cout << a[i] << " ";
-	}
-
-	for (int k = 0; k < n; ++k) {
-            cout << res[k] << " ";
-	}
-
-	return 0;
-
+	 
+ int n, c, i;
+ 
+ cout << "Введите размер массива" << endl;
+ 
+ cin >> n;
+ 
+ cout << "Введите элементы массива" <<
+ endl;
+ 
+ int *a = new int[n];
+ 
+ for(i = 0; i < n; i++){
+ 
+ cin >> a[i];
+ 
+ } 
+ 
+ cout << endl;
+ 
+ for (int i = 0; i < n/2; i++) {
+ 
+ c = a[n-1-i];
+ 
+ a[n-1-i] = a[i]; 
+ 
+ a[i] = c;
+ 
+ }
+ 
+ for(i = 0; i < n; i++){
+ 
+ cout << a[i] << ' ';
+ 
+ } 
+ 
+ return 0;
+ 
 }
