e78772fb493e378439ac23a52eec3954ea0bf0e2
nastyaz122 <115534094+nastyaz122@users.noreply.github.com>
2022-10-12 18:44:48 +0300

    Update Laba2.cpp

--- Nastya Zotova/Laba_2/Laba2.cpp
+++ Nastya Zotova/Laba_2/Laba2.cpp
@@ -4,7 +4,7 @@ using namespace std;
 
 int main()
 {
-	int n, i;
+	int n, i, c;
 
 	cout << "VVedite razmer massiva" << endl;
 
@@ -12,7 +12,7 @@ int main()
 
 	cout << "VVedite elementi massiva" << endl;
 	
-	int *a = new int[n];
+	int *a = new int[n]; // *а - указатель на выделенный участок памяти под массив целочисленного типа, new -операция выделения свободной памяти для хранения n целочисленных переменных
 
 	for ( i = 0; i < n; i++) {
 
@@ -22,9 +22,20 @@ int main()
 
 	cout << endl;
 
-	for (int i = n - 1; i >= 0; i--) {
+	for (int i = 0; i < n/2; i++) {
 
-		cout << a[i] << " ";
+		c = a[n-1-i];
+		a[n-1-i] = a[i];
+		a[i] = c;        // делим массив пополам, работаем с левой частью, уменьшаем размер массива на 1, также с индексом и присваиваем значение через новую переменную
+
 	}
+	
+	for (int i = 0; i < n; i++){
+		
+		cout << a[i] << " ";
+		}
+		
+	delete[] a; // освобождение выделенной памяти 
+	
 	return 0;
 }
