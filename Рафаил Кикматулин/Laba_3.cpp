f64c253517211680d7c1e399e8c9135c3f2bf516
Rafail <Raf_0707@mail.ru>
2022-11-26 08:41:49 +0300

    Update Laba_3.cpp

--- Рафаил Кикматулин/Laba_3.cpp
+++ Рафаил Кикматулин/Laba_3.cpp
@@ -1,33 +1,40 @@
-#include <iostream>
-
-using namespace std;
-
-void prost_chisla(int n, int res[]) {
-    int k = 0;
-    int divv = 2;
-    while (k < 3) {
-        while (n % divv == 0) {
-            cout << divv << " ";
-            res[k] = divv;
-            n = n / divv;
-            k++;
-            
-            if (k == 2) {
-                divv = n;
-            }
-        }
-        
-    divv++;
-        
-    }
-   
-} 
-
-
-int main() {
-    int n;
-    cin >> n;
-    int res[3];
+#include <iostream> 
+ 
+using namespace std; 
+ 
+void prost_chisla(int n, int res[]) { 
+    int k = 0; 
+    int divv = 2; 
+    while (k < 3) { 
+        while (n % divv == 0) { 
+            //cout << divv << " "; 
+            res[k] = divv; 
+            n = n / divv; 
+            k++; 
+             
+            if (k == 2) { 
+                divv = n; 
+            } 
+        } 
+         
+    divv++; 
+         
+    } 
+    
+}  
+ 
+ 
+int main() { 
+    int n; 
+    cin >> n; 
+    int res[3]; 
     prost_chisla(n, res);
-    return 0;
+    
+    int a = sizeof(res) / sizeof(res[0]); 
+    
+    for (int i = 0; i < a; ++i) {
+        cout << res[i] << " ";
+    }
+    
+    return 0; 
 }
