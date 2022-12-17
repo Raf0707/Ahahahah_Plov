a6aafc5d1992e124836c3f3f234d9ecdf683b571
Rafail <Raf_0707@mail.ru>
2022-12-17 14:17:35 +0300

    Update Laba_5.cpp

--- Рафаил Кикматулин/Laba_5.cpp
+++ Рафаил Кикматулин/Laba_5.cpp
@@ -28,19 +28,26 @@ int main() {
     cout << "Вернитесь в нашу эру" << endl;
   }
 
-  if (visocosnick(year) || year == -2) {
-    cout << "Високосный" << endl;
-    cout << "Введите номер месяца от 1 до 12" << endl;
-
-    int mon;
-    cin >> mon;
+  cout << "Введите номер месяца" << endl;
+  int mon;
+  cin >> mon;
   
     switch(mon) {
         case 1:
             cout << "В январе 31 день" << endl;
             break;
         case 2:
+          if (visocosnick(year) || year == -2) {
+            if (year == -2) {
+              cout << "Вы ввели обозначенине високосного года" << endl;
+              cout << "В феврале 29 дней" << endl;
+            } else {
+            cout << year << " год - Високосный" << endl;
             cout << "В феврале 29 дней" << endl;
+            }
+          } else {
+          cout << "В феврале 28 дней" << endl;
+          }
             break;
         case 3:
             cout << "В марте 31 день" << endl;
@@ -79,60 +86,6 @@ int main() {
       }
 
     cout << endl;
-    
-  } else if (!(visocosnick(year)) || year == -2) {
-      cout << "Не високосный" << endl;
-      cout << "Введите номер месяца от 1 до 12" << endl;
-    
-      int mon;
-      cin >> mon;
-    
-      switch(mon) {
-          case 1:
-            cout << "В январе 31 день" << endl;
-            break;
-          case 2:
-              cout << "В феврале 28 дней" << endl;
-              break;
-          case 3:
-              cout << "В марте 31 день" << endl;
-              break;
-          case 4:
-              cout << "В апреле 30 дней" << endl;
-              break;
-          case 5:
-              cout << "В мае 31 день" << endl;
-              break;
-          case 6:
-              cout << "В июне 30 дней" << endl;
-              break;
-          case 7:
-              cout << "В июле 31 день" << endl;
-              break;
-          case 8:
-              cout << "В августе 31 день" << endl;
-              break;
-          case 9:
-              cout << "В сентябре 30 дней" << endl;
-              break;
-          case 10:
-              cout << "В октябре 31 день" << endl;
-              break;
-          case 11:
-              cout << "В ноябре 30 дней" << endl;
-              break;
-          case 12:
-              cout << "В декабре 31 день" << endl;
-              break;
-          default:
-              cout << "введите номер месяца от 1 до 12" << endl;
-              break;
-
-      }
-    
-      cout << endl;
-    
-    }
 
   return 0;
 
