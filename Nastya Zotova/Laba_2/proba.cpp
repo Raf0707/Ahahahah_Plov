0d9ff9a54a2d251a06158e2e8ecc6e491b1e2c9d
nastyaz122 <115534094+nastyaz122@users.noreply.github.com>
2022-12-21 16:18:20 +0300

    Update proba.cpp

--- Nastya Zotova/Laba_2/proba.cpp
+++ Nastya Zotova/Laba_2/proba.cpp
@@ -4,23 +4,33 @@ enum units { mm, cm, dm, m, km };
 int main()
 {
 	units num = mm;
-
+	int n = 50;
 		switch (num)
 		{
 		case mm:
 			cout << "Result in mm" << endl;
+			n = n * 1000;
+			cout << n << endl;
 			break;
 		case cm:
 			cout << "Result in cm" << endl;
+			n = n * 100;
+			cout << n << endl;
 			break;
 		case dm:
 			cout << "Result in dm" << endl;
+			n = n * 10;
+			cout << n << endl;
 			break;
 		case m:
 			cout << "Result in m" << endl;
+			n = n * 1;
+			cout << n << endl;
 			break;
 		case km:
 			cout << "Result in km" << endl;
+			n = n / 1000;
+			cout << n << endl;
 			break;
 		}
 
