1cdb2df3ca5162c2335e35b7a9b81f937c05af2e
nastyaz122 <115534094+nastyaz122@users.noreply.github.com>
2022-12-21 15:53:22 +0300

    Update proba.cpp

--- Nastya Zotova/Laba_2/proba.cpp
+++ Nastya Zotova/Laba_2/proba.cpp
@@ -8,30 +8,35 @@ int main()
 		{
 		case mm:
 			cout << "it works!" << endl;
+			break;
 		}
 	units num2 = cm;
 		switch (num2)
 		{
 		case cm:
 			cout << "it works x2!" << endl;
+			break;
 		}
 	units num3 = dm;
 		switch (num3)
 		{
 		case dm:
-			cout << "it works! x3!" << endl;
+			cout << "it works x3!" << endl;
+			break;
 		}
 	units num4 = m;
 		switch (num4)
 		{
 		case m:
 			cout << "it works x4!" << endl;
+			break;
 		}
 	units num5 = km;
 		switch (num5)
 		{
 		case km:
 			cout << "it works x5!" << endl;
+			break;
 		}
 
 	return 0;
