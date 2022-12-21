4369883c51d7ad8e2eaf75280fef6bb16d018ebb
nastyaz122 <115534094+nastyaz122@users.noreply.github.com>
2022-12-21 16:06:05 +0300

    Update proba.cpp

--- Nastya Zotova/Laba_2/proba.cpp
+++ Nastya Zotova/Laba_2/proba.cpp
@@ -3,39 +3,24 @@ using namespace std;
 enum units { mm, cm, dm, m, km };
 int main()
 {
-	units num1 = mm;
-		switch (num1)
+	units num = mm;
+
+		switch (num)
 		{
 		case mm:
-			cout << "it works!" << endl;
+			cout << "Result in mm" << endl;
 			break;
-		}
-	units num2 = cm;
-		switch (num2)
-		{
 		case cm:
-			cout << "it works x2!" << endl;
+			cout << "Result in cm" << endl;
 			break;
-		}
-	units num3 = dm;
-		switch (num3)
-		{
 		case dm:
-			cout << "it works x3!" << endl;
+			cout << "Result in dm" << endl;
 			break;
-		}
-	units num4 = m;
-		switch (num4)
-		{
 		case m:
-			cout << "it works x4!" << endl;
+			cout << "Result in m" << endl;
 			break;
-		}
-	units num5 = km;
-		switch (num5)
-		{
 		case km:
-			cout << "it works x5!" << endl;
+			cout << "Result in km" << endl;
 			break;
 		}
 
